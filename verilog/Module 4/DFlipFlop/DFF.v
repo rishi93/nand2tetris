af555f6bea6f3c84459d081bd2faadd8f6baca9d
Rajarishi Devarajan <rishi_devan@mail.com>
2025-12-25 23:00:35 +0100

    clean up Module 4

--- verilog/Module 4/DFlipFlop/DFF.v
+++
@@ -1,9 +0,0 @@
-module DFF(
-    input wire clk,
-    input wire d,
-    output reg q
-);
-    always @(posedge clk) begin
-        q <= d;
-    end
-endmodule;
--- verilog/Module 4/Register/Register.v
+++ verilog/Module 4/Register/Register.v
@@ -1,18 +1,9 @@
 module Register(
     input wire [15:0] in,
     input wire load, clk,
-    output wire [15:0] out
+    output reg [15:0] out
 );
-    genvar i;
-    
-    generate
-        for (i = 0; i < 16; i = i + 1) begin: bits
-            Bit bit(
-                .in(in[i]),
-                .clk(clk),
-                .load(load),
-                .out(out[i])
-            );
-        end
-    endgenerate
+    always @(posedge clk) begin
+        if (load) out <= in;
+    end
 endmodule;
